b4d6eb3384df681d0e433b6e2eda617bc0f75e91
John Steel <john@jskw.dev>
2022-07-23 15:30:51 -0400

    Remove mapping to customers

--- src/main/java/entity/PhoneEntity.java
+++ src/main/java/entity/PhoneEntity.java
@@ -1,14 +1,10 @@
 package entity;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.ToString;
 
 import javax.persistence.*;
 import javax.validation.constraints.Pattern;
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
 @Data
@@ -21,20 +17,10 @@ public class PhoneEntity {
     @Pattern(regexp = "^(?:\\+?\\d{1}\\s?)?\\(?(\\d{3})\\)?-?\\s?(\\d{3})-?\\s?(\\d{4})$")
     String phoneNumber;
 
-    @JsonBackReference
-    @EqualsAndHashCode.Exclude
-    @ToString.Exclude
-    private Set<CustomerEntity> customers = new HashSet<>();
-
     @Id
     @GeneratedValue
     @Column(name = "PID", nullable = false)
     public Long getId() {
         return id;
-    }
-
-    @ManyToMany(mappedBy="phones")
-    public Set<CustomerEntity> getCustomers() {
-        return customers;
     }
 }
